135f08f4c65fdf87adf0593b574fcade54fa00b4
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-07 21:44:44 +0100

    commit

--- app/src/main/java/com/example/home3d/Piece.java
+++ app/src/main/java/com/example/home3d/Piece.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class Piece extends AppCompatActivity {
 
+    protected Mur mur1, mur2, mur3, mur4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
