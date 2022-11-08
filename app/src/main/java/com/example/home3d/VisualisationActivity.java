3ea798f320d7db087f7016f5e2d857c0dcf68128
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-08 20:31:26 +0100

    commit

---
+++ app/src/main/java/com/example/home3d/VisualisationActivity.java
@@ -0,0 +1,18 @@
+package com.example.home3d;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class VisualisationActivity extends AppCompatActivity {
+
+    protected Button visualisation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualisation);
+
+    }
+}
