37067ed53b6f6f5c05332c166bdf3c761f2b069b
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-08 20:29:56 +0100

    commit

--- app/src/main/java/com/example/home3d/Batiment.java
+++ app/src/main/java/com/example/home3d/Batiment.java
@@ -6,13 +6,9 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 
-public class Batiment extends AppCompatActivity {
+public class Batiment  {
 
     protected ArrayList<Piece>pieces;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-    }
+
 }
