6e83a921ef0c1428ecac312915b6cbd187f55a9e
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-08 20:30:18 +0100

    commit

--- app/src/main/java/com/example/home3d/Mur.java
+++ app/src/main/java/com/example/home3d/Mur.java
@@ -6,13 +6,9 @@ import android.os.Bundle;
 
 import java.util.ArrayList;
 
-public class Mur extends AppCompatActivity {
+public class Mur {
 
     protected ArrayList<Acces>acces;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mur);
-    }
+
 }
