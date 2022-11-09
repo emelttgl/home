480bbbac030551c932602fd6efe53b798a9b111f
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-10 00:40:59 +0100

    commit

--- app/src/main/java/com/example/home3d/Piece.java
+++ app/src/main/java/com/example/home3d/Piece.java
@@ -8,12 +8,26 @@ public class Piece{
 
     protected String nom;
     protected Mur mur1, mur2, mur3, mur4;
+    protected boolean isSelected;
 
-    public Piece(String s){
+    public Piece(String s, boolean isSelected){
         this.nom=s;
+        this.isSelected=isSelected;
     }
 
     public String getNom (){
         return nom;
     }
+
+    public void setNom(String nom){this.nom=nom;}
+
+    public boolean isSelected(){ return isSelected();}
+
+    public void setSelected(boolean selected) { isSelected=selected;}
+
+    public String toString(){
+        return "Piece{"+"nom='"+nom+'\''+", isSelected="+isSelected+'}';
+    }
+
+
 }
