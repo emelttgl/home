76ffcffdc8c9b59dc02b6907c4a949fff63a2c3a
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-18 19:57:30 +0100

    commit

--- app/src/main/java/com/example/home3d/ConstructionActivity.java
+++ app/src/main/java/com/example/home3d/ConstructionActivity.java
@@ -1,29 +1,33 @@
 package com.example.home3d;
 
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
-import android.database.Cursor;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.text.InputType;
-import android.view.LayoutInflater;
+import android.util.JsonToken;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class ConstructionActivity extends AppCompatActivity {
 
@@ -31,18 +35,34 @@ public class ConstructionActivity extends AppCompatActivity {
     protected ArrayList<Piece> pieces;
     protected Adapter adapter;
     protected Button ajouter;
+    protected Button sauvegarder;
+    protected ActivityResultLauncher<Intent>launcher;
+    protected SharedPreferences sharedPreferences;
 
 
-    protected LinearLayout linearLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_construction);
+        pieces = new ArrayList<>();
+        ajouter = findViewById(R.id.ajouter);
+        recyclerView = findViewById(R.id.recyclerview);
+        sauvegarder = findViewById(R.id.save);
 
-        ajouter=findViewById(R.id.ajouter);
-        recyclerView=(RecyclerView) findViewById(R.id.recyclerview);
 
+        sauvegarder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sharedPreferences=getSharedPreferences("data",MODE_PRIVATE);
+               SharedPreferences.Editor editor = sharedPreferences.edit();
+
+               //editor.putInt();
+               editor.apply();
+            }
+        });
+        
         ajouter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,13 +76,14 @@ public class ConstructionActivity extends AppCompatActivity {
                 dialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                       String name = editText.getText().toString();
-                       Toast.makeText(ConstructionActivity.this, "Nouvelle pièce ajoutée: "+name,Toast.LENGTH_LONG).show();
-                        Piece piece= new Piece(name,false);
+                        String name = editText.getText().toString();
+                        Toast.makeText(ConstructionActivity.this, "Nouvelle pièce ajoutée: " + name, Toast.LENGTH_LONG).show();
+                        Piece piece = new Piece(name, false);
                         pieces.add(piece);
                         recyclerView.setLayoutManager(new LinearLayoutManager(ConstructionActivity.this));
-                        adapter=new Adapter(ConstructionActivity.this,pieces);
+                        adapter = new Adapter(ConstructionActivity.this, pieces);
                         recyclerView.setAdapter(adapter);
+
                     }
                 });
 
@@ -73,10 +94,9 @@ public class ConstructionActivity extends AppCompatActivity {
                     }
                 });
                 dialog.show();
+
             }
         });
-        pieces=new ArrayList<>();
     }
-
 
 }
