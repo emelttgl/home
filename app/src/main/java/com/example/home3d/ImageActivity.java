0a4ff3f894b865cfff30820a952882395934f9f4
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-18 19:57:37 +0100

    commit

--- app/src/main/java/com/example/home3d/ImageActivity.java
+++ app/src/main/java/com/example/home3d/ImageActivity.java
@@ -7,12 +7,14 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -31,6 +33,7 @@ public class ImageActivity extends AppCompatActivity {
     protected ActivityResultLauncher<Intent> launcherS;
     protected ImageView ouest,nord,sud,est;
     protected Button valider;
+    protected ImageButton Ouest,Nord,Sud,Est;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,48 +41,28 @@ public class ImageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image);
 
         valider=findViewById(R.id.buttonVal);
+
+        Ouest=findViewById(R.id.imageButtonOuest);
+        Nord=findViewById(R.id.imageButtonNord);
+        Sud=findViewById(R.id.imageButtonSud);
+        Est=findViewById(R.id.imageButtonEst);
+
         ouest = findViewById(R.id.imageView7);
         nord = findViewById(R.id.imageView);
         est = findViewById(R.id.imageView6);
         sud = findViewById(R.id.imageView8);
 
+
         valider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ImageActivity.this.finish();
+
+
             }
         });
-        ouest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                launcherO.launch(i);
-            }
-        });
-        launcherO = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
-                        Toast.makeText(ImageActivity.this,"taille"+bitmap.getHeight() ,Toast.LENGTH_SHORT).show();
-                        FileOutputStream fos = null;
-                        try {
-                            fos = openFileOutput("imageO.data", MODE_PRIVATE);
-                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
 
-                        } catch (FileNotFoundException e) {
-                            throw new RuntimeException(e);
-                        }
-                        try {
-                            fos.flush();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
 
-                    }
-                }
-        );
 
         sud.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -176,29 +159,12 @@ public class ImageActivity extends AppCompatActivity {
                     }
                 }
         );
-recupImageO();
-recupImageE();
-recupImageN();
-recupImageS();
+        recupImageO();
+        recupImageE();
+        recupImageN();
+        recupImageS();
+        prendrePhotoO();
     }
-
-/*
-        FileInputStream fis = null;
-        try {
-            fis = openFileInput("image.data");
-            Bitmap bm = BitmapFactory.decodeStream(fis);
-            nord.setImageBitmap(bm);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        FileInputStream fis2 = null;
-        try {
-            fis = openFileInput("image.data");
-            Bitmap bm = BitmapFactory.decodeStream(fis);
-            sud.setImageBitmap(bm);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }*/
 
     public void recupImageO() {
         FileInputStream fis = null;
@@ -240,4 +206,54 @@ recupImageS();
             e.printStackTrace();
         }
     }
+
+    public void prendrePhotoN(){
+
     }
+
+    public void prendrePhotoE(){
+
+    }
+
+    public void prendrePhotoS(){
+
+    }
+
+    public void prendrePhotoO(){
+        Ouest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                launcherO.launch(i);
+            }
+        });
+        launcherO = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Bitmap bitmap = (Bitmap) result.getData().getExtras().get("data");
+                        Toast.makeText(ImageActivity.this,"taille image : "+bitmap.getHeight() ,Toast.LENGTH_SHORT).show();
+                        FileOutputStream fos = null;
+                        try {
+                            fos = openFileOutput("imageO.data", MODE_PRIVATE);
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+
+                        } catch (FileNotFoundException e) {
+                            throw new RuntimeException(e);
+                        }
+                        try {
+                            fos.flush();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                }
+        );
+    }
+
+    public void modifierPhoto(){
+
+    }
+}
