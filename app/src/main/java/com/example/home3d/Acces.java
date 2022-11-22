0dca68d7a4c9890258c7f8c1f595b1ea10769e2c
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-22 16:47:33 +0100

    commit

--- app/src/main/java/com/example/home3d/Acces.java
+++
@@ -1,8 +0,0 @@
-package com.example.home3d;
-
-
-public class Acces {
-
-    protected Piece piece1, piece2;
-
-}
---
+++ app/src/main/java/com/example/home3d/monde/Acces.java
@@ -0,0 +1,10 @@
+package com.example.home3d.monde;
+
+
+import java.io.Serializable;
+
+public class Acces implements Serializable {
+
+    protected Piece piece1, piece2;
+
+}
