2b00cd064a533edcbfdaa2816d78e41c6ebd7f45
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-22 16:49:33 +0100

    commit

---
+++ app/src/main/java/com/example/home3d/monde/Mur.java
@@ -0,0 +1,15 @@
+package com.example.home3d.monde;
+
+
+
+import com.example.home3d.monde.Acces;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Mur implements Serializable {
+
+    protected ArrayList<Acces>acces;
+
+
+}
