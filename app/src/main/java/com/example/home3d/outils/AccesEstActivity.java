1022b7461c91160a3edfd7ba48a11641b9cbebcb
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-11-22 16:49:45 +0100

    commit

---
+++ app/src/main/java/com/example/home3d/outils/AccesEstActivity.java
@@ -0,0 +1,88 @@
+package com.example.home3d.outils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.example.home3d.R;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class AccesEstActivity extends AppCompatActivity {
+
+    protected ImageView est;
+    protected SurfaceView surfaceView;
+    protected SurfaceHolder surfaceHolder;
+    protected DrawRectangle dr;
+    protected int x0, y0, x1, y1;
+    protected RelativeLayout relativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acces_est);
+
+        est=findViewById(R.id.imageViewEst);
+        surfaceView = findViewById(R.id.surfaceViewEst);
+        relativeLayout=findViewById(R.id.relativeL);
+
+        surfaceHolder = surfaceView.getHolder();
+        surfaceView.setZOrderOnTop(true);
+        surfaceHolder.setFormat(PixelFormat.TRANSPARENT);
+
+
+
+        est.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                relativeLayout.removeView(dr);
+                if (event.getPointerCount() == 2) {
+                    if (event.getActionIndex() == MotionEvent.ACTION_DOWN) {
+                        x0 = (int) event.getX(0);
+                        y0 = (int) event.getY(0);
+
+                        Log.i("SelectActivity", "x0 : " + x0);
+                        Log.i("SelectActivity", "y0 : " + y0);
+                    }
+                    x1 = (int) event.getX(1);
+                    y1 = (int) event.getY(1);
+                    Log.i("SelectActivity", "x1 : " + x1);
+                    Log.i("SelectActivity", "y1 : " + y1);
+
+                }
+                //Log.i("SelectActivity", "rect " + rect.toString());
+                Canvas canvas = new Canvas();
+                Paint paint = new Paint();
+
+                dr = new DrawRectangle(AccesEstActivity.this, paint, canvas, x0,y0,x1,y1);
+                relativeLayout.addView(dr);
+                return true;
+            }
+        });
+
+        recupImageE();
+    }
+    public void recupImageE() {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput("imageE.data");
+            Bitmap bm = BitmapFactory.decodeStream(fis);
+            this.est.setImageBitmap(bm);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
