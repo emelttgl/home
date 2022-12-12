d8cab5a2a3f3953f2c5ef9fccc4b1352aec426f7
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-12-13 00:26:53 +0100

    commit

--- app/src/main/java/com/example/home3d/outils/AccesOuestActivity.java
+++ app/src/main/java/com/example/home3d/outils/AccesOuestActivity.java
@@ -1,34 +1,51 @@
 package com.example.home3d.outils;
 
+import static com.example.home3d.outils.ConstructionActivity.BATIMENT_KEY;
+import static com.example.home3d.outils.ConstructionActivity.PIECES_KEY;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.home3d.R;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class AccesOuestActivity extends AppCompatActivity {
-
+    public static final String Message="message";
     protected ImageView ouest;
     protected SurfaceView surfaceView;
     protected SurfaceHolder surfaceHolder;
     protected DrawRectangle dr;
     protected int x0, y0, x1, y1;
     protected RelativeLayout relativeLayout;
+    protected Button valider;
+    protected TextView textView;
+    protected String Souest;
+    protected ActivityResultLauncher<Intent> launcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +53,16 @@ public class AccesOuestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_acces);
 
         ouest = findViewById(R.id.imageViewOuest);
-        surfaceView = findViewById(R.id.surfaceView);
+        surfaceView = findViewById(R.id.surfaceViewOuest);
         relativeLayout=findViewById(R.id.relativeL);
 
         surfaceHolder = surfaceView.getHolder();
         surfaceView.setZOrderOnTop(true);
         surfaceHolder.setFormat(PixelFormat.TRANSPARENT);
-
-
+        valider=findViewById(R.id.valider);
+        textView=findViewById(R.id.textView8);
+        Souest="";
+        textView.setVisibility(View.INVISIBLE);
 
         ouest.setOnTouchListener((v, event) -> {
             relativeLayout.removeView(dr);
@@ -70,14 +89,41 @@ public class AccesOuestActivity extends AppCompatActivity {
             return true;
         });
 
-        recupImageO();
+        /*launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                    this.Souest = (String) result.getData().getSerializableExtra(PIECES_KEY);
+                });*/
+
+            valider.setOnClickListener(v -> {
+            Bitmap bitmap=((BitmapDrawable) ouest.getDrawable()).getBitmap();
+            Souest=bitmapToString(bitmap);
+            textView.setText(bitmapToString(bitmap));
+            textView.setVisibility(View.INVISIBLE);
+
+            Log.i("message",bitmapToString(bitmap));
+
+            Intent intent = new Intent(this, VisualisationActivity.class);
+            intent.putExtra(BATIMENT_KEY,Souest);
+
+            //startActivity(intent);
+            //finish();
+        });
+
+            recupImageO();
+    }
+
+    private String bitmapToString(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,50,stream);
+        byte[] bytes=stream.toByteArray();
+        return Base64.encodeToString(bytes,0);
+
     }
 
 
     public void recupImageO() {
         FileInputStream fis = null;
         try {
-            fis = openFileInput("imageO.data");
+            fis = openFileInput("image3.data");
             Bitmap bm = BitmapFactory.decodeStream(fis);
             this.ouest.setImageBitmap(bm);
         } catch (FileNotFoundException e) {
