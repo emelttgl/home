15e16284841104bd1515a425ecf1c5a1baf2af8c
emeltatoglu <emel.tatoglu3@etu.univ-lorraine.fr>
2022-12-13 00:28:22 +0100

    commit

--- app/src/main/java/com/example/home3d/outils/ConstructionActivity.java
+++ app/src/main/java/com/example/home3d/outils/ConstructionActivity.java
@@ -8,13 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.home3d.R;
@@ -28,37 +32,41 @@ import java.util.ArrayList;
 public class ConstructionActivity extends AppCompatActivity {
     final static public String BATIMENT_KEY="Envoyer";
     final static public String PIECES_KEY="Recuperer";
+    final static public String Piece ="Piece";
     protected RecyclerView recyclerView;
     protected ArrayList<Piece> pieces;
     protected Adapter adapter;
+    protected Adapter.ViewHolder view;
     protected Button ajouter;
     protected Button sauvegarder;
     protected ActivityResultLauncher<Intent>launcher;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_construction);
-        pieces = new ArrayList<Piece>();
+        pieces = new ArrayList<>();
         //batiment = (Batiment) getIntent().getSerializableExtra(PIECES_KEY);
-        //pieces = (ArrayList<Piece>) getIntent().getSerializableExtra(PIECES_KEY);
+      // pieces = (ArrayList<Piece>) getIntent().getSerializableExtra(Piece);
         //pieces = pieces.getPieces();
         ajouter = findViewById(R.id.ajouter);
         recyclerView = findViewById(R.id.recyclerview);
         sauvegarder = findViewById(R.id.save);
+
 
         SharedPreferences sharedPreference = getSharedPreferences("DATA", MODE_PRIVATE);
         Gson gson = new Gson();
         String json = sharedPreference.getString("studata", null);
         Type type = new TypeToken<ArrayList<Piece>>(){}.getType();
         pieces=gson.fromJson(json,type);
+
         if(pieces==null){
-            pieces=new ArrayList<>();
+          pieces=new ArrayList<>();
         }
 
         buildRecyclerView();
-
 
         ajouter.setOnClickListener(v -> {
             AlertDialog.Builder dialog = new AlertDialog.Builder(ConstructionActivity.this);
@@ -71,6 +79,9 @@ public class ConstructionActivity extends AppCompatActivity {
             dialog.setPositiveButton("ok", (dialog1, which) -> {
                 String name = editText.getText().toString();
                 Toast.makeText(ConstructionActivity.this, "Nouvelle pièce ajoutée: " + name, Toast.LENGTH_LONG).show();
+               // int nextid=adapter.getItemCount();
+               // getIntent().getSerializableExtra(Piece);
+               // adapter.setSelection(nextid++);
                 Piece piece = new Piece(name, null, null, null, null);
                 pieces.add(piece);
                 buildRecyclerView();
@@ -95,14 +106,14 @@ public class ConstructionActivity extends AppCompatActivity {
             String jsonn= gsonn.toJson(pieces);
             editor.putString("studata",jsonn);
             editor.apply();
+            Toast.makeText(this, "Pieces enregistrées", Toast.LENGTH_SHORT).show();
+
             finish();
         });
 
-       // launcher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),result -> {
-          //  pieces= (ArrayList<Piece>) getIntent().getSerializableExtra(PIECES_KEY);
-       // });
-
-
+       /* launcher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),result -> {
+            pieces= (ArrayList<Piece>) getIntent().getSerializableExtra(Piece);
+        });*/
         }
 
 
